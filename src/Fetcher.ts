b0c86642746570cea760c3d78d6ee0366ebdab73
약풍 <yakpoong@inflab.com>
2025-08-20 21:28:37 +0900

    Revert "chore: 1.0.9"
    This reverts commit 4a16c8c952a4c280317dcceecc2c67bd7e89dfdf.

--- src/Fetcher.ts
+++ src/Fetcher.ts
@@ -43,7 +43,6 @@ export class Fetcher {
     }
 
     const response = await fetch(url, {
-      mode: "navigate",
       headers: {
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36",
         "Cache-Control" : "no-cache",
