96e5e504c8f4dab5b5f2f739f880be1e07f5a4c3
약풍 <yakpoong@inflab.com>
2025-08-20 15:34:10 +0900

    chore: 1.0.10

--- src/Fetcher.ts
+++ src/Fetcher.ts
@@ -43,7 +43,7 @@ export class Fetcher {
     }
 
     const response = await fetch(url, {
-      mode: "navigate",
+      mode: "no-cors",
       headers: {
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36",
         "Cache-Control" : "no-cache",
